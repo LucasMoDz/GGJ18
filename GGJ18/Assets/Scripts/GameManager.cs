6c7b3867000959649784d901a5deb99ae4f511c9
Andrea Mercuri <andrea.mercuri21@gmail.com>
2018-01-27 15:57:38 +0100

    - Creato GameManager con metodo per avviare il movimento della navicella e per estrarre una razza random - Creato Spaceship Handler con metodi per settare la sprite in base alla razza estratta e per fare il movimento della navicella - Inseriti placeholder grafici nella scena

---
+++ GGJ18/Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public float timeToReachEarth = 20f;
+    
+    // Use this for initialization
+    void Start () {
+
+        //Chiama il metodo che estrae random una razza
+        RandomRace();
+
+        //Chiama metodo che avvia la generazione simboli
+
+        //Chiama metodo per attivare gli slider
+
+        //Fa partire il movimento della navicella
+        SpaceshipEvents.moveToPosition(timeToReachEarth);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void RandomRace()
+    {
+        int randomValue = Random.Range(0, 3);
+        string raceName = "null";
+
+        switch (randomValue)
+        {
+            case 0:
+                raceName = "rept";
+                break;
+
+            case 1:
+                raceName = "robot";
+                break;
+
+            case 2:
+                raceName = "bigHead";
+                break;
+        }
+        Debug.Log(raceName);
+        SpaceshipEvents.setSprite(raceName);
+
+    }
+
+
+}
