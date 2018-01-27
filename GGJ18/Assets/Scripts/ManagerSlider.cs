a5b71276e7e7e3dcee054b85d119c3eecc3a810e
LucasMoDz <lucasmodzita@gmail.com>
2018-01-27 15:49:09 +0100

    - Manager slider - Created package and slider prefabs

---
+++ GGJ18/Assets/Scripts/ManagerSlider.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using Random = UnityEngine.Random;
+
+public static class SliderEvents
+{
+    public static Action<bool> SliderActivation;
+}
+
+public class ManagerSlider : MonoBehaviour
+{
+    public RectTransform range;
+    public float maxPositionY = 144;
+    public float minPositionY = -144;
+    public float ratioRangeSlider = .2f;
+
+    private Slider slider;
+    private Coroutine activeCoroutine;
+    private float totalPositionRange;
+    private float normalizedMaxRange, normalizedMinRange;
+    private bool sliderIsActive;
+    
+    private void Awake()
+    {
+        if (range == null)
+        {
+            Debug.LogError("\"Range\" is null, please fix it.\n");
+            return;
+        }
+        
+        range.localPosition = Vector3.zero;
+        totalPositionRange = maxPositionY + Mathf.Abs(minPositionY);
+        
+        range.sizeDelta = new Vector2(range.sizeDelta.x, this.GetComponent<RectTransform>().sizeDelta.y * ratioRangeSlider);
+
+        slider = this.GetComponent<Slider>();
+
+        SliderEvents.SliderActivation += _value =>
+        {
+            sliderIsActive = _value;
+
+            if (!_value || activeCoroutine != null)
+                return;
+
+            activeCoroutine = StartCoroutine(StartSliderMovementCO());
+        };
+    }
+
+    private IEnumerator StartSliderMovementCO()
+    {
+        float step, seconds, startPosition, targetPosition;
+        float halfHeigth = totalPositionRange * (ratioRangeSlider / 2);
+        Vector3 temp_position = range.localPosition;
+
+        StartCoroutine(CheckRangeValidationCO());
+
+        sliderIsActive = true;
+
+        while (sliderIsActive)
+        {
+            step = 0;
+            seconds = Random.Range(.5f, 1.5f);
+            startPosition = range.localPosition.y;
+            targetPosition = Random.Range(minPositionY, maxPositionY);
+            
+            yield return new WaitForSecondsRealtime(seconds);
+
+            while (step < 1.0f)
+            {
+                step += Time.deltaTime / seconds;
+                temp_position.y = Mathf.Lerp(startPosition, targetPosition, step);
+                range.localPosition = temp_position;
+
+                normalizedMaxRange = (temp_position.y + maxPositionY + halfHeigth) / totalPositionRange;
+                normalizedMinRange = (temp_position.y + maxPositionY - halfHeigth) / totalPositionRange;
+
+                yield return null;
+            }
+
+            yield return null;
+        }
+
+        activeCoroutine = null;
+    }
+
+    private IEnumerator CheckRangeValidationCO()
+    {
+        while (sliderIsActive)
+        {
+            if (slider.value < normalizedMaxRange && slider.value > normalizedMinRange)
+            {
+                SignalReceived();
+            }
+            else
+            {
+                SignalNotReceived();
+            }
+
+            yield return null;
+        }
+    }
+
+    private void SignalReceived()
+    {
+        Debug.Log("Signal received\n");
+    }
+
+    private void SignalNotReceived()
+    {
+        Debug.Log("Signal not received\n");
+    }
+
+    // To test
+    private void Update()
+    {
+#if UNITY_EDITOR
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            SliderEvents.SliderActivation(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.B))
+        {
+            SliderEvents.SliderActivation(false);
+        }
+#endif
+    }
+}
